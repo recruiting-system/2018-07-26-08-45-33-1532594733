acf9beddbfc575ebb1ce05296098b479aa559245
JunoChow <41833300+JunoChow@users.noreply.github.com>
2018-08-03 00:43:47 +0800

    Add files via upload
    test

---
+++ main.js
@@ -0,0 +1,10 @@
+// Write your cade below:
+module.exports = function main() {
+  var result = a % b
+  //声明变量result，并添加实现a和b得余数的代码
+    return result;
+//添加调用main()的代码，传入参数
+var a=prompt('请输入被除数')
+var b=prompt('请输入除数')
+console.log("余数是:" + main(a,b));
+  }
